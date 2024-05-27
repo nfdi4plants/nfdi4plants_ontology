40faf35b01ba1f178eecb60ba0cb4c51c09d9cca
Hannah-Doerpholz <108814050+Hannah-Doerpholz@users.noreply.github.com>
2024-05-27 14:56:38 +0200

    Create verify_purls.py

---
+++ .github/workflows/verify_purls.py
@@ -0,0 +1,20 @@
+import requests
+
+input_file = 'missing_purls.txt'
+output_file = 'missing_purls_verified.txt'
+base_url = 'http://purl.org/nfdi4plants/ontology/dpbo/'
+
+def check_url_status(url):
+    try:
+        response = requests.head(url, allow_redirects=True)
+        return response.status_code == 200
+    except requests.RequestException as e:
+        print(f"Request failed: {e}")
+        return False
+
+with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
+    for line in infile:
+        id = line.strip()
+        url = f"{base_url}{id}"
+        if not check_url_status(url):
+            outfile.write(f"{id}\n")
